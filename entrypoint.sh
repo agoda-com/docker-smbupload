41c28f04293d8049fbc5868c97567a846ae226e5
Vitaliy Gorbachov <Vitaliy.Gorbachov@agoda.com>
2022-01-13 11:02:59 +0700

    latest changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-FILE_NAME_WIN=$(echo ${FILE_NAME} | tr '[:$]' '_')
 echo "uploading ${FILE_NAME} to //${SMB_HOST}/${SMB_SHARE}/${SMB_DIR}/${FILE_NAME_WIN}"
 echo -e "username = ${SMB_USERNAME}\npassword = ${SMB_PASSWORD}\ndomain = ${SMB_DOMAIN}" > /tmp/auth
-smbclient //${SMB_HOST}/${SMB_SHARE} -A /tmp/auth -c "mkdir ${SMB_DIR}; put /upload/${FILE_NAME} ${SMB_DIR}/${FILE_NAME_WIN}; exit"
+smbclient //${SMB_HOST}/${SMB_SHARE} -A /tmp/auth -c "prompt OFF; recurse ON; mkdir ${SMB_DIR}; cd ${SMB_DIR}; lcd /upload; mput *; exit"
